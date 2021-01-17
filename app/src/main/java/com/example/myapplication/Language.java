e3f358fac322ab07117758629db8fc020f134032
AqilahS <aqilahsyazaniaziz@gmail.com>
2021-01-17 18:53:24 +0800

    Finder Kit

--- app/src/main/java/com/example/myapplication/Language.java
+++ app/src/main/java/com/example/myapplication/Language.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Language extends AppCompatActivity {
 
@@ -10,5 +13,48 @@ public class Language extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
+    }
+
+    public static class MainActivity extends AppCompatActivity {
+            public Button mapbutton;
+            public Button infobutton;
+            public Button aboutbutton;
+
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                mapbutton = (Button) findViewById(R.id.mapbtn);
+                infobutton = (Button) findViewById(R.id.infobtn);
+                aboutbutton = (Button) findViewById(R.id.aboutbtn);
+
+
+                aboutbutton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this,About.class);
+                        startActivity(intent);
+                    }
+                });
+
+                infobutton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this,Info.class);
+                        startActivity(intent);
+                    }
+                });
+
+                mapbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this,Map.class);
+                    startActivity(intent);
+                }
+            });
+
+        }
     }
 }
